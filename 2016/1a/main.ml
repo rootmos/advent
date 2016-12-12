15bb7e8bbed89b0f36200b3ccaa7f06b81aa3007
Gustav Behm <gustav.behm@magine.com>
2016-12-12 19:45:07 +0100

    Handle string splitting in the harness

--- 2016/1a/main.ml
+++ 2016/1a/main.ml
@@ -1,6 +1,9 @@
 open Core_kernel.Std
+open List
 
-let go = print_string l
+let go l =
+  let raw_dirs = String.split ~on:',' l |> map ~f:String.strip in
+  iter ~f:print_string raw_dirs
 
 let () = 
   In_channel.with_file "input.txt" ~f:(fun f -> In_channel.input_all f |> go)
