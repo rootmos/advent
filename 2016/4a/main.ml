0be707b3911e7bf9e10d0401777f137d416e6e31
Gustav Behm <gustav.behm@magine.com>
2016-12-13 16:46:10 +0100

    Changes after Fabians code review

--- 2016/4a/main.ml
+++ 2016/4a/main.ml
@@ -13,25 +13,23 @@ let split_line l =
 
 let checksum s =
   String.to_list s
-    |> filter ~f:(fun c -> c <> '-')
+    |> filter ~f:((<>) '-')
     |> sort ~cmp:Pervasives.compare
     |> group ~break:(<>)
-    |> map ~f:(fun xs -> (length xs, hd_exn xs))
+    >>| (fun xs -> (length xs, hd_exn xs))
     |> sort ~cmp:(fun (i, c) (i', c') ->
         let c1 = (Pervasives.compare i' i) in
         let c2 = (Pervasives.compare c c') in
         if c1 <> 0 then c1 else c2)
-    |> map ~f:(fun (l, c) -> printf "(%d, %c)\n" l c; (l, c))
-    |> map ~f:snd
-    |> (fun l -> take l 5)
+    >>| snd
+    |> Fn.flip take 5
     |> String.of_char_list
-    |> (fun x -> printf "%s\n" x; x)
 
 let go ls =
   map ~f:split_line ls
     |> filter ~f:(fun (n, _, c) -> checksum n = c)
-    |> map ~f:(fun (_, s, _) -> s)
+    >>| (fun (_, s, _) -> s)
     |> fold ~init:0 ~f:(+)
     |> printf "%d\n"
 
-let () = In_channel.with_file "input.txt" ~f:(fun f -> In_channel.input_lines f |> go)
+let () = In_channel.read_lines "input.txt" |> go
