32fc212a1144e18a1a92b3db0dab7dbd711e9408
Gustav Behm <gustav.behm@magine.com>
2016-12-17 19:34:42 +0100

    Add solution to 13a

--- 2016/13a/main.ml
+++ 2016/13a/main.ml
@@ -2,7 +2,9 @@ open Core_kernel.Std
 open List
 open Printf
 
-let favorite_number = 10
+module S = Set.Make(struct include Tuple.Make(Int)(Int) include Tuple.Comparable(Int)(Int) end)
+
+let favorite_number = 1350
 
 let bit_count n =
   let rec inner acc m = if m = 0 then acc else inner (acc + (m land 1)) (m lsr 1) in
@@ -25,21 +27,24 @@ let print_maze w h path =
 let walk_to x y =
   let are_we_there_yet path = hd_exn path = (x, y) in
 
-  let steps (x, y) =
+  let steps visited (x, y) =
     let ps = [(x-1,y); (x+1,y); (x,y-1); (x,y+1)] in
-    filter ps ~f:is_open in
+    filter ps ~f:(fun p -> not (S.mem visited p) && is_open p) in
 
-  let rec inner paths =
+  let rec inner visited paths =
     match find paths are_we_there_yet with
     | Some p -> p
     | None ->
-        let paths' = map paths ~f:(fun path -> steps (hd_exn path) >>| (fun p -> p :: path)) in
-        inner (concat paths') in
-  inner [[(1,1)]]
+        let visited' = fold paths ~init:visited ~f:(fun vs path -> S.add vs (hd_exn path)) in
+        let paths' = map paths ~f:(fun path ->
+          steps visited' (hd_exn path) >>| (fun p -> p :: path)) in
+        inner visited' (concat paths') in
+
+  inner S.empty [[(1,1)]]
 
 let steps path = length path - 1
 
 let () =
-  let path = walk_to 7 4 in
-  print_maze 25 10 path;
+  let path = walk_to 31 39 in
+  print_maze 45 45 path;
   path |> steps |> printf "%d\n"
