7eb60df30c164e72a3d8e7c5e34f0c04561bb604
Gustav Behm <gustav.behm@magine.com>
2016-12-18 09:23:15 +0100

    Possible solution to 14b (slow)

---
+++ 2016/14b/main.ml
@@ -0,0 +1,32 @@
+open Core_kernel.Std
+open List
+open Printf
+
+let md5 s = Digest.string s |> Digest.to_hex
+
+let has_repetition n s =
+  let rec inner c m = function
+    | c' :: cs when c = c' -> let m' = m + 1 in if m' = n then Some c else inner c m' cs
+    | c' :: cs -> inner c' 1 cs
+    | [] -> None in
+  inner ' ' 1 (String.to_list s)
+
+let hash seed i =
+  let inner j = (Fn.apply_n_times ~n:2017 md5) @@ sprintf "%s%d" seed j in
+  Memo.general inner i
+
+let make_stream seed =
+  let raw_stream start = Sequence.unfold ~init:start ~f:(fun i ->
+    Some ((i, hash seed i), i + 1)) in
+
+  let has_five_fold_continuation c start =
+    let pattern = sprintf "%c%c%c%c%c" c c c c c in
+    Sequence.take (raw_stream start) 1000 |> Sequence.find ~f:(fun (i, s) ->
+      String.substr_index s ~pattern:pattern |> Option.is_some) |> Option.is_some in
+
+  Sequence.filter (raw_stream 0) ~f:(fun (i, s) ->
+    Option.(has_repetition 3 s >>| (fun c -> has_five_fold_continuation c (i + 1))) |> ((=) (Some true)))
+
+let () =
+  let stream = make_stream "qzyelonm" in
+  Sequence.(take stream 64 |> iter ~f:(fun (i, s) -> printf "%d %s\n" i s))
